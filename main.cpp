c63e1a7b9f0c3f13ad3fffdf761cbf9f27b48173
Marcin <262101@student.pwr.edu.pl>
2021-10-27 16:26:17 +0200

    V0.2

--- main.cpp
+++ main.cpp
@@ -4,9 +4,24 @@
 #include <QLocale>
 #include <QTranslator>
 
+#include <QCoreApplication>
+
+#include "xlsxdocument.h"
+#include "xlsxchartsheet.h"
+#include "xlsxcellrange.h"
+#include "xlsxchart.h"
+#include "xlsxrichstring.h"
+#include "xlsxworkbook.h"
+using namespace QXlsx;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+       QXlsx::Document xlsx;
+       xlsx.write("A1", "Hello Qt!"); // write "Hello Qt!" to cell(A,1). it's shared string.
+       xlsx.saveAs("Test.xlsx"); // save the document as 'Test.xlsx'
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
